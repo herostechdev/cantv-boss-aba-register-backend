60bbfcfadec1557077e81b2ea485fb3812661b4d
Frank Paredes <devrepublik@gmail.com>
2023-11-08 00:25:57 -0300

    legal documents

--- src/get-legal-documents/get-legal-documents.service.ts
+++ src/get-legal-documents/get-legal-documents.service.ts
@@ -4,6 +4,7 @@ import * as path from 'path';
 import { ExceptionsService } from 'src/system/infrastructure/services/exceptions.service';
 import { DocumentsConfigurationService } from 'src/system/configuration/documents/documents-configuration.service';
 import { IGetLegalDocuments } from './get-legal-documents-response.interface';
+import { CustomNotFoundException } from 'src/system/infrastructure/exceptions/custom-exceptions/custom-not-found-exception';
 
 @Injectable()
 export class GetLegalDocumentsService extends ExceptionsService {
@@ -24,18 +25,32 @@ export class GetLegalDocumentsService extends ExceptionsService {
   }
 
   private getContractDocument(): string {
-    const filePath = path.join(
-      this.documentsConfigurationService.legalDocumentsFolder,
-      this.documentsConfigurationService.contractDocumentName,
-    );
-    return fs.readFileSync(filePath, 'utf8');
+    try {
+      const filePath = path.join(
+        this.documentsConfigurationService.legalDocumentsFolder,
+        this.documentsConfigurationService.contractDocumentName,
+      );
+      return fs.readFileSync(filePath, 'utf8');
+    } catch (error) {
+      throw new CustomNotFoundException({
+        objectOrError: 'No existe el documento: CONTRATO',
+        guid: '93f817de-d72e-491b-8ecc-0d8d9d2b1e01',
+      });
+    }
   }
 
   private getTermsAndConditionsDocument(): string {
-    const filePath = path.join(
-      this.documentsConfigurationService.legalDocumentsFolder,
-      this.documentsConfigurationService.termsAndConditionsDocumentName,
-    );
-    return fs.readFileSync(filePath, 'utf8');
+    try {
+      const filePath = path.join(
+        this.documentsConfigurationService.legalDocumentsFolder,
+        this.documentsConfigurationService.termsAndConditionsDocumentName,
+      );
+      return fs.readFileSync(filePath, 'utf8');
+    } catch (error) {
+      throw new CustomNotFoundException({
+        objectOrError: 'No existe el documento: TÉRMINOS Y CONDICIONES',
+        guid: '88ce02fc-5928-4712-b799-962ee95e0d6c',
+      });
+    }
   }
 }
