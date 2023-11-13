65440898117911ef2a3162b12eea4247ceb48281
Frank Paredes <devrepublik@gmail.com>
2023-11-13 03:54:36 -0300

    logger

--- src/validate-technical-feasibility/get-data-from-requests/get-data-from-requests-response.interface.ts
+++ src/validate-technical-feasibility/get-data-from-requests/get-data-from-requests-response.interface.ts
@@ -1,13 +1,14 @@
 import { IStatusResponse } from 'src/responses/status-response.interface';
 import { GetDataFromRequestsStatusConstants } from './get-data-from-requests-status.constants';
+import { IPhoneNumber } from 'src/responses/phone-number.interface';
 
 export interface IGetDataFromRequestsResponse
-  extends IStatusResponse<GetDataFromRequestsStatusConstants> {
+  extends IPhoneNumber,
+    IStatusResponse<GetDataFromRequestsStatusConstants> {
   id: string;
   firstname: string;
   lastname: string;
   email: string;
-  phonenumber: string;
   address1: string;
   address2: string;
   city: string;
