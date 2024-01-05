e2b766a0fccd2dffe7943e473c146f232f86d9c5
Frank Paredes <devrepublik@gmail.com>
2024-01-05 12:28:01 -0300

    reorganization

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,24 +3,24 @@ import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
-import { ApplicationConfigurationModule } from './system/configuration/application/application-configuration.module';
-import { ThrottlerConfigurationModule } from './system/configuration/throttler/throttler-configuration.module';
-import { ThrottlerConfigurationService } from './system/configuration/throttler/throttler-configuration.service';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { ApplicationConfigurationModule } from './system/configuration/application/application-configuration.module';
+import { DocumentsConfigurationModule } from './system/configuration/documents/documents-configuration.module';
+import { IntegrationsConfigurationModule } from './system/configuration/pic/integrations-configuration.module';
+import { OracleConfigurationModule } from './system/configuration/oracle/oracle-configuration.module';
+import { SecurityConfigurationModule } from './system/configuration/security/security-configuration.module';
+import { ThrottlerConfigurationModule } from './system/configuration/throttler/throttler-configuration.module';
+import { ThrottlerConfigurationService } from './system/configuration/throttler/throttler-configuration.service';
+
 import { AbaRegisterModule } from './aba-register-flow/aba-register.module';
 import { BossApiModule } from './raw/boss-api/boss-api-raw.module';
+import { EncryptionModule } from './system/infrastructure/security/encryption/encryption.module';
 import { PICModule } from './raw/pic/pic.module';
 import { StoredProceduresRawModule } from './raw/stored-procedures/stored-procedure-raw.module';
 
 import { ConfirmRegistrationModule } from './confirm-registration/confirm-registration.module';
-import { DocumentsConfigurationModule } from './system/configuration/documents/documents-configuration.module';
-import { EncryptionModule } from './system/infrastructure/security/encryption/encryption.module';
-import { IntegrationsConfigurationModule } from './system/configuration/pic/integrations-configuration.module';
-import { OracleConfigurationModule } from './system/configuration/oracle/oracle-configuration.module';
-import { SecurityConfigurationModule } from './system/configuration/security/security-configuration.module';
 import { ValidateTechnicalFeasibilityModule } from './validate-technical-feasibility/validate-technical-feasibility.module';
 
 @Module({
@@ -37,15 +37,15 @@ import { ValidateTechnicalFeasibilityModule } from './validate-technical-feasibi
     }),
 
     ApplicationConfigurationModule,
+    DocumentsConfigurationModule,
+    IntegrationsConfigurationModule,
+    OracleConfigurationModule,
+    SecurityConfigurationModule,
 
     AbaRegisterModule,
     BossApiModule,
-    DocumentsConfigurationModule,
     EncryptionModule,
-    IntegrationsConfigurationModule,
-    OracleConfigurationModule,
     PICModule,
-    SecurityConfigurationModule,
     StoredProceduresRawModule,
 
     ConfirmRegistrationModule,
